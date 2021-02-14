936f9ce0f355cf1f72147d8b4ce2fe31523f4373
Artem <artemdoubov@hotmail.com>
2021-02-14 15:10:00 -0500

    Deliverable 2 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,9 @@
+# Deliverable 1
+
 # 3. Import prereqs
 library(dplyr) 
 
-# 4. Load CSV data file as a dataframe
+# 4. Load CSV data file
 df_MechaCarMPGData <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 # 5 Multiple linear regression function
@@ -9,3 +11,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # 6 Summarize linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=df_MechaCarMPGData))
+
+
+# Deliverable 2
+
+# 2. Load CSV data file
+tbl_SuspCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# 3. Create total_summary dataframe
+df_total_summary <- tbl_SuspCoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# 4. Create lot_summary dataframe
+df_lot_summary <- tbl_SuspCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
