548480305133a53710c07d4e12ab03dc895800b0
Artem <artemdoubov@hotmail.com>
2021-02-14 16:09:18 -0500

    Deliverable 3 code / images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,17 @@ df_total_summary <- tbl_SuspCoil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 
 # 4. Create lot_summary dataframe
 df_lot_summary <- tbl_SuspCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+
+
+# Deliverable 3
+
+# 1.One-sample t-test against population mean of 1500
+t.test(tbl_SuspCoil$PSI, mu=1500)
+
+# 2 One-sample t-tests per manufacturing lot against population mean of 1500
+# Lot 1
+t.test(filter(tbl_SuspCoil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+# Lot 2
+t.test(filter(tbl_SuspCoil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+# Lot 3
+t.test(filter(tbl_SuspCoil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
